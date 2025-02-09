9f635602542d10d24e5b35bf80859adadad08559
schacherlnate <139157234+schacherlnate@users.noreply.github.com>
2025-02-08 22:15:00 -0600

    make grid squares interactive

--- scripts.js
+++ scripts.js
@@ -1,8 +1,29 @@
-const sketchScreen = document.querySelector("#sketch-screen");
-let gridSize = document.querySelector("input");
-let square;
-for (let i = 0; i < gridSize.value; i++) {
-    square = document.createElement("div");
-    square.classList.add("square");
-    sketchScreen.appendChild(square);
+function generateGrid(n) {
+    const sketchScreen = document.querySelector("#sketch-screen");
+    if (sketchScreen.hasChildNodes()) {
+        sketchScreen.replaceChildren();
+    }
+
+    let square;
+    for (let i = 0; i < n**2; i++) {
+        square = document.createElement("div");
+        square.classList.add("square");
+        square.style.width = `calc(100%/${n})`;
+        square.style.height = `calc(100%/${n})`;
+        square.textContent=i;
+        square.addEventListener("mouseenter", function(e) {
+            //knew correct target, each square had the listener, but value for square still saved
+            e.target.style.backgroundColor="black";
+        });
+        sketchScreen.appendChild(square);
+    }
 }
+
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", function() {
+    const gridSize = document.querySelector("input");
+    generateGrid(gridSize.value);
+});
+
+let defaultSize = 16
+generateGrid(defaultSize);
